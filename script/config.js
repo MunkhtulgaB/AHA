8006308aecc05c9f79f01a3f3669af9165a59573
Munkhtulga <munkhtulga.battogtokh@gmail.com>
2023-04-22 11:53:23 +0100

    Add configs

---
+++ script/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyDw4O1Pz6ZXJzswEoteLkuV5-SFnNK0odY",
+    authDomain: "aha-21c8e.firebaseapp.com",
+    databaseURL: "https://aha-21c8e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "aha-21c8e",
+    storageBucket: "aha-21c8e.appspot.com",
+    messagingSenderId: "251008592400",
+    appId: "1:251008592400:web:d03a655227e64a96553895",
+    measurementId: "G-DZWCLMWDK8"
+  };
+
+export { config }
