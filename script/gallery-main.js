5f43339c5a56b443cca6e5d4ab50b8af6afee4bd
Munkhtulga <munkhtulga.battogtokh@gmail.com>
2023-04-22 14:59:36 +0100

    Remove the file upload option from gallery page

--- script/gallery-main.js
+++ script/gallery-main.js
@@ -55,16 +55,3 @@ onAuthStateChanged(auth, (user) => {
         });
     }
 });
-
-
-const img_input = document.querySelector("#img");
-img_input.addEventListener("change", function() {
-    const img_name = img_input.value;
-    const reader = new FileReader();
-    reader.addEventListener("load", function() {
-        const imgBase64 = reader.result;
-        // document.querySelector("#img_container").src = imgBase64;
-        writeImageData(img_name, imgBase64);
-    });
-    reader.readAsDataURL(this.files[0]);
-})
