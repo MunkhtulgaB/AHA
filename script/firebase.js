c44febe19d0dc742a23b3e6ceadf930f3ffedb7c
Munkhtulga <munkhtulga.battogtokh@gmail.com>
2023-04-22 13:09:56 +0100

    Move authentication to gallery page

--- script/firebase.js
+++ script/firebase.js
@@ -1,72 +1,12 @@
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
 import { getDatabase, onChildAdded, onChildRemoved, ref, set, push, child, get } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js'
-import { GoogleAuthProvider, getAuth, signInWithRedirect, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
 import { config } from "./config.js"
-import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js";
 
-//add your credentials from firebase project
 const firebaseConfig = config;
-
 const app = initializeApp(firebaseConfig);
-const db = getDatabase();
 const auth = getAuth(app);
-
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-
-        // ...
-
-        writeUserData(user.uid, user.displayName, user.email, user.photoURL)
-        console.log("getting data")
-        getData(user.uid);
-        
-        const imagesRef = ref(db, "images");
-        onChildAdded(imagesRef, function(data) {
-          const imgData = data.val();
-          console.log("loaded image", imgData.name);
-          const imgBase64 = imgData.imageBase64;
-          $("#img_wall").append(
-            `<img id="${data.key}" src="${imgBase64}" width="300px;"/>`
-          )
-        });
-        onChildRemoved(imagesRef, function(data) {
-          $(`img#${data.key}`).remove();
-        });
-
-    } else {
-        // User is signed out
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-
-
-
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-            console.log(errorMessage)
-        });
-    }
-});
-
+const db = getDatabase();
 
 
 
--- script/gallery-main.js
+++ script/gallery-main.js
@@ -1,4 +1,61 @@
 import { writeImageData } from "./firebase.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+import { getDatabase, onChildAdded, onChildRemoved, ref, set, push, child, get } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js'
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
+import { config } from "./config.js"
+import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js";
+
+
+const firebaseConfig = config;
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
+
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const imagesRef = ref(db, "images");
+        onChildAdded(imagesRef, function(data) {
+          const imgData = data.val();
+          console.log("loaded image", imgData.name);
+          const imgBase64 = imgData.imageBase64;
+          $("#img_wall").append(
+            `<img id="${data.key}" src="${imgBase64}" width="300px;"/>`
+          )
+        });
+        onChildRemoved(imagesRef, function(data) {
+          $(`img#${data.key}`).remove();
+        });
+
+    } else {
+        // User is signed out
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+
+
+
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+            console.log(errorMessage)
+        });
+    }
+});
+
 
 const img_input = document.querySelector("#img");
 img_input.addEventListener("change", function() {
